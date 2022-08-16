cbcd64717edb6a31392ce3cd473c61e1735849c6
James Gay <jumo@Jamess-MacBook-Air.local>
2022-08-16 18:21:17 +0800

    new

---
+++ src/main/java/com/ssfMiniProject/BmiTrackerApp/BmiTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ssfMiniProject.BmiTrackerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssfMiniProject/BmiTrackerApp/BmiTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssfMiniProject.BmiTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
